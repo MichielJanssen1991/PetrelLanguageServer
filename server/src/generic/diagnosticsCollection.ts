f6f5058c479b45409b4d57ec7b14f9ffb20bf357
Koos Luijk <koos.luijk@philips.com>
2022-01-17 10:52:59 +0100

    idea to add unique identifiers to switch easily between error/warn/info

--- server/src/generic/diagnosticsCollection.ts
+++ server/src/generic/diagnosticsCollection.ts
@@ -1,7 +1,25 @@
 import { Diagnostic, DiagnosticSeverity, Range } from 'vscode-languageserver';
 
+export type DiagnosticDefinition = {
+	severity: DiagnosticSeverity
+}
+
 export class DiagnosticsCollection {
 	private diagnostics: Diagnostic[] = [];
+	private diagnosticDefinitions: Record<string, DiagnosticDefinition> = {
+		// STRC = Structure
+		// idea is to have different kind of id's. Start with a string of 3 or 4 letters, then a number of length 5. Reference errors could be REF00001
+		"STRC00001": { "severity": DiagnosticSeverity.Error },
+		"STRC00002": { "severity": DiagnosticSeverity.Error },
+		"STRC00003": { "severity": DiagnosticSeverity.Error },
+		"STRC00004": { "severity": DiagnosticSeverity.Error },
+		"STRC00005": { "severity": DiagnosticSeverity.Error },
+		"STRC00006": { "severity": DiagnosticSeverity.Error },
+		"STRC00007": { "severity": DiagnosticSeverity.Error },
+		"STRC00008": { "severity": DiagnosticSeverity.Error },
+		"STRC00009": { "severity": DiagnosticSeverity.Error },
+		"STRC00010": { "severity": DiagnosticSeverity.Error },
+	}
 
 	public getDiagnostics() {
 		return this.diagnostics;
@@ -21,6 +39,23 @@ export class DiagnosticsCollection {
 
 	protected addError(range: Range, message: string) {
 		this.addDiagnostic(range, message, DiagnosticSeverity.Error);
+	}
+
+	protected addMessage(range: Range, id: string, message: string) {
+		switch(this.diagnosticDefinitions[id].severity){
+			case DiagnosticSeverity.Error:
+				this.addError(range, message);
+				break;
+			case DiagnosticSeverity.Warning:
+				this.addWarning(range, message);
+				break;
+			case DiagnosticSeverity.Information:
+				this.addInformation(range, message);
+				break;
+			default:
+				// nothing
+				break;
+		}
 	}
 
 	protected addDiagnostic(range: Range, message: string, severity: DiagnosticSeverity) {
